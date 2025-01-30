d52ceec8c996a1447660e2ca4ffc5cc9a27d5f98
MohamedHesham2024 <mohmedhesham2024@gmail.com>
2025-01-30 13:38:05 +0200

    FEAT:handle cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 import { SwaggerConfig } from './config/swagger';
 import { RabbitMqConfigModule } from './config/rabbitmq-config.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,13 @@ async function bootstrap() {
     }),
   );
   await RabbitMqConfigModule.setup(app);
+  app.enableCors({
+    origin: '*', // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+});
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
